1f1dd390eb58fa8451a04fa926062e00b0c58f6f
AnkushDiverseCoder <luciferkingofhell1902@gmail.com>
2022-10-29 12:13:09 +0530

    initial commit

---
+++ components/Footer.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    
+      <div className="grid grid-cols-1 md:grid-cols-4 sm:grid-cols-2 mx-auto px-32 py-14 bg-gray-100 ">
+        <div className="space-y-4 text-xs text-gray-800">
+          <h5 className="font-bold">ABOUT</h5>
+          <p>How Airbnb Works</p>
+          <p>NewsRoom</p>
+          <p>Investors</p>
+          <p>Airbnb Plus</p>
+          <p>Airbnb Luxe</p>
+        </div>
+
+        <div className="space-y-4 text-xs text-gray-800">
+          <h5 className="font-bold">COMMUNITY</h5>
+          <p>How Airbnb Works</p>
+          <p>NewsRoom</p>
+          <p>Investors</p>
+          <p>Airbnb Plus</p>
+          <p>Airbnb Luxe</p>
+        </div>
+
+        <div className="space-y-4 text-xs text-gray-800">
+          <h5 className="font-bold">HOST</h5>
+          <p>How Airbnb Works</p>
+          <p>NewsRoom</p>
+          <p>Investors</p>
+          <p>Airbnb Plus</p>
+          <p>Airbnb Luxe</p>
+        </div>
+
+        <div className="space-y-4 text-xs text-gray-800">
+          <h5 className="font-bold">ABOUT</h5>
+          <p>How Airbnb Works</p>
+          <p>NewsRoom</p>
+          <p>Investors</p>
+          <p>Airbnb Plus</p>
+          <p>Airbnb Luxe</p>
+        </div>
+      </div>
+  );
+};
+
+export default Footer;
---
+++ components/Header.js
@@ -0,0 +1,90 @@
+import Image from "next/image";
+import { SearchIcon } from "@heroicons/react/solid";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+      {/* left */}
+      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+        <Image
+          src="/logo.png"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
+      </div>
+
+      {/* middle */}
+      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+        <input
+          type="text"
+          placeholder="Start Your Search"
+          className="inline pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400"
+        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-6 h-6 bg-red-400 text-white rounded-full p-1 hidden md:inline-flex md:mx-2"
+        >
+          <path
+            fillRule="evenodd"
+            d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </div>
+
+      {/* right */}
+      <div className="flex items-center space-x-4 justify-end">
+        <p className="hidden md:inline cursor-pointer" >Become a host</p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
+          />
+        </svg>
+
+        <div className="flex items-center space-x-2 border-2 justify-end text-gray-500 rounded-full p-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6 cursor-pointer"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+            />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 cursor-pointer"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+          </svg>
+        </div>
+      </div>
+    </header>
+  );
+};
+export default Header;
